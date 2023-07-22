93d24a558a3ff3be8cd4b2e3dce18eea1f9063b2
Gibrano Derenz <gibranoderenz123@gmail.com>
2023-07-22 15:37:48 +0700

    Handle role based sheet

--- src/components/modules/HomeModule/module-elements/PostSheet/index.tsx
+++ src/components/modules/HomeModule/module-elements/PostSheet/index.tsx
@@ -19,10 +19,13 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Switch } from "@/components/ui/switch";
 
 export const PostSheet: React.FC = () => {
   const { user } = useAuthContext();
   const { openPostSheet, setOpenPostSheet, post } = useHomeContext();
+
+  const findMentor = async () => {};
 
   return (
     <Sheet
@@ -56,8 +59,10 @@ export const PostSheet: React.FC = () => {
         <Tabs defaultValue="story">
           <TabsList>
             <TabsTrigger value="story">Story</TabsTrigger>
-            {post?.user.id === user?.id ? (
+            {post?.user.id === user?.id && !user?.is_mentor ? (
               <TabsTrigger value="mentor">Mentor</TabsTrigger>
+            ) : post?.user.id === user?.id && user?.is_mentor ? (
+              <TabsTrigger value="mentees">Mentees</TabsTrigger>
             ) : (
               <TabsTrigger value="profile">Profile</TabsTrigger>
             )}
@@ -110,27 +115,15 @@ export const PostSheet: React.FC = () => {
             </div>
           </TabsContent>
           <TabsContent value="mentor">
-            <Card>
-              <CardHeader>
-                <div className="flex items-center gap-2">
-                  <Avatar
-                    className={
-                      "w-[40px] h-[40px] hover:shadow-lg transition duration-75 cursor-pointer"
-                    }
-                  >
-                    <AvatarImage src={""} />
-                    <AvatarFallback>PC</AvatarFallback>
-                  </Avatar>
-                  <div className="flex flex-col gap-1">
-                    <CardTitle>Card Title</CardTitle>
-                    <CardDescription>Card Description</CardDescription>
-                  </div>
-                </div>
-              </CardHeader>
-              <CardContent>
-                <span>Card Content</span>
-              </CardContent>
-            </Card>
+            <div className="flex flex-col gap-4">
+              <span>
+                You have not been matched with any mentors. Click the button
+                below to be guided to learn your region language! It's worth the
+                shot.
+              </span>
+
+              <Button onClick={findMentor}>Find me a mentor!</Button>
+            </div>
           </TabsContent>
           <TabsContent value="profile">
             <Card>
@@ -154,6 +147,18 @@ export const PostSheet: React.FC = () => {
                 <span>Know about {post?.user.username} more!</span>
               </CardContent>
             </Card>
+          </TabsContent>
+          <TabsContent value="mentees">
+            <div className="flex flex-col gap-4">
+              <span>
+                You have not been matched with any mentees. You will be
+                automatically matched with any mentees until you choose not to
+                be available. You can toggle the setting anytime!
+              </span>
+              <div className="flex items-center gap-2">
+                <Switch /> <span>Open to Accept Mentees</span>
+              </div>
+            </div>
           </TabsContent>
         </Tabs>
       </SheetContent>
